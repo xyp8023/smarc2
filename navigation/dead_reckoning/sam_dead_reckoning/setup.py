66d1f3082ccfec28091b9a95599b2a3b4081e9c7
julian valdez <r.julianvaldez@gmail.com>
2024-04-11 12:10:29 +0200

    new package

---
+++ navigation/dead_reckoning/sam_dead_reckoning/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'sam_dead_reckoning'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='julian',
+    maintainer_email='r.julianvaldez@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
